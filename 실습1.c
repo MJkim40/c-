643c0d3fb7da90b115d145032794e9325203e7b8
MJkim40 <minji200404@gmail.com>
2025-01-21 10:44:43 +0900

    실습1.c

--- 실습1.c
+++ 실습1.c
@@ -1,24 +1,24 @@
-//#include <stdio.h>
-//
-//
-//int main() {
-//	int* pi;
-//	double* pf;
-//	char* pc;
-//	pi = (int*)malloc(sizeof(int));
-//	pf = (double*)malloc(sizeof(double));
-//	pc = (char*)malloc(sizeof(char));
-//	if (pi == NULL || pf == NULL || pc == NULL) {
-//		printf("µ¿Àû ¸Þ¸ð¸® ÇÒ´ç ¿À·ù\n");
-//		exit(1);
-//	}
-//
-//	*pi = 100;
-//	*pf = 3.14;
-//	*pc = 'a';
-//
-//	free(pi);
-//	free(pf);
-//	free(pc);
-//	return 0;
-//}
+#include <stdio.h>
+
+
+int main() {
+	int* pi;
+	double* pf;
+	char* pc;
+	pi = (int*)malloc(sizeof(int));
+	pf = (double*)malloc(sizeof(double));
+	pc = (char*)malloc(sizeof(char));
+	if (pi == NULL || pf == NULL || pc == NULL) {
+		printf("ÂµÂ¿Ã€Ã» Â¸ÃžÂ¸Ã°Â¸Â® Ã‡Ã’Â´Ã§ Â¿Ã€Â·Ã¹\n");
+		exit(1);
+	}
+
+	*pi = 100;
+	*pf = 3.14;
+	*pc = 'a';
+
+	free(pi);
+	free(pf);
+	free(pc);
+	return 0;
+}
