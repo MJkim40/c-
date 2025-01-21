54da630b42875e3a3ad27f9851b95509424b4daf
MJkim40 <minji200404@gmail.com>
2025-01-21 10:39:21 +0900

    #01_multiplication_table.c

--- #01_multiplication_table.c
+++ #01_multiplication_table.c
@@ -1,15 +1,15 @@
-//#include <stdio.h>
-//
-//int main()
-//{
-//	for (int i = 1; i < 10; i += 3) {
-//		for (int j = 0; j < 10; j++) {
-//			for (int k = 0; k < 3; k++) {
-//				if (j == 0) { printf("%d´Ü\t\t", i + k); }
-//				else { printf("%d * %d = %2d\t", i + k, j, (i + k) * j); }
-//			}
-//			printf("\n");
-//		}
-//		printf("\n\n");
-//	}
-//}
+#include <stdio.h>
+
+int main()
+{
+	for (int i = 1; i < 10; i += 3) {
+		for (int j = 0; j < 10; j++) {
+			for (int k = 0; k < 3; k++) {
+				if (j == 0) { printf("%dÂ´Ãœ\t\t", i + k); }
+				else { printf("%d * %d = %2d\t", i + k, j, (i + k) * j); }
+			}
+			printf("\n");
+		}
+		printf("\n\n");
+	}
+}
