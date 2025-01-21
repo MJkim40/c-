5b44595e856c43764f874a3e603a4a13b93b5745
MJkim40 <minji200404@gmail.com>
2025-01-21 10:41:15 +0900

    #02_struct_pointer.c

--- #02_struct_pointer.c
+++ #02_struct_pointer.c
@@ -1,23 +1,23 @@
-//#include <stdio.h>
-//
-//struct human {
-//	char name[10];
-//	float power;
-//};
-//
-//void attack(struct human *H1, struct human *H2) {
-//	if (H1->power > H2->power) {
-//		printf("�⵿ %s\n", H1->name);
-//	}
-//	else {
-//		printf("�⵿ %s\n", H2->name);
-//	}
-//}
-//int main() {
-//	struct human hero1 = { "Grace", 8.2 };
-//	struct human hero2 = {"Oscar", 9.3};
-//	
-//	attack(&hero1, &hero2);
-//
-//	return 0;
-//}
+#include <stdio.h>
+
+struct human {
+	char name[10];
+	float power;
+};
+
+void attack(struct human *H1, struct human *H2) {
+	if (H1->power > H2->power) {
+		printf("Ãâµ¿ %s\n", H1->name);
+	}
+	else {
+		printf("Ãâµ¿ %s\n", H2->name);
+	}
+}
+int main() {
+	struct human hero1 = { "Grace", 8.2 };
+	struct human hero2 = {"Oscar", 9.3};
+	
+	attack(&hero1, &hero2);
+
+	return 0;
+}
