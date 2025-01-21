f5a5803ba9176e5e6162da08f950bd86a6bb2b0c
MJkim40 <minji200404@gmail.com>
2025-01-21 10:44:51 +0900

    실습2.c

--- 실습2.c
+++ 실습2.c
@@ -1,20 +1,20 @@
-//#include <stdio.h>
-//#include <stdlib.h>
-//
-//int main() {
-//	char* pc;
-//
-//	pc = (char*)malloc(100 * sizeof(char));
-//	if (pc == NULL) {
-//		printf("�޸� �Ҵ� ����\n");
-//		return 1;
-//	}
-//	for (int i = 0; i < 100; i++) {
-//		pc[i] = 'a';
-//		for (int i = 0; i < 100; i++) {
-//			printf("%c", pc[i]);
-//		}
-//	}
-//	free(pc);
-//	return 0;
-//}
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	char* pc;
+
+	pc = (char*)malloc(100 * sizeof(char));
+	if (pc == NULL) {
+		printf("¸Þ¸ð¸® ÇÒ´ç ¿À·ù\n");
+		return 1;
+	}
+	for (int i = 0; i < 100; i++) {
+		pc[i] = 'a';
+		for (int i = 0; i < 100; i++) {
+			printf("%c", pc[i]);
+		}
+	}
+	free(pc);
+	return 0;
+}
