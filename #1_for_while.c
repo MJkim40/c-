1af47f1ce2f50054863be941e45e6aaa3dd5a837
MJkim40 <minji200404@gmail.com>
2025-01-21 10:42:45 +0900

    #1_for_while.c

--- #1_for_while.c
+++ #1_for_while.c
@@ -1,12 +1,12 @@
-//#include <stdio.h>
-//
-//int main() {
-//	float celsius;
-//	int fahr = 0;
-//	while (fahr < 101) {
-//		celsius = (fahr - 32) * 5 / 9;
-//		printf("%3d F = %5.1f C\n", fahr, celsius);
-//		fahr = fahr + 10;
-//	}
-//	return 0;
-//}
+#include <stdio.h>
+
+int main() {
+	float celsius;
+	int fahr = 0;
+	while (fahr < 101) {
+		celsius = (fahr - 32) * 5 / 9;
+		printf("%3d F = %5.1f C\n", fahr, celsius);
+		fahr = fahr + 10;
+	}
+	return 0;
+}
