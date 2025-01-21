4704d6e3632fba06fd38c7a7f855bcd5189ccaca
MJkim40 <minji200404@gmail.com>
2025-01-21 10:40:11 +0900

    #02_find_primes.c

--- #02_find_primes.c
+++ #02_find_primes.c
@@ -1,27 +1,27 @@
-//#include <stdio.h>
-//
-//int main()
-//{
-//	int number;
-//	int prime;
-//	int count=0;
-//	printf("¼Ò¼ö¸¦ Ã£À» ¹üÀ§¸¦ ÀÔ·Â: ");
-//	scanf_s("%d", &number);
-//	for (int i = 2; i <= number; i++) {
-//		prime = 1;
-//		for (int j = 2; j < i; j++) {
-//			if (i % j == 0) {
-//				prime = 0;
-//				break;
-//			}
-//		}
-//		if (prime == 1) {
-//			printf("%d\t", i);
-//			count++;
-//			if (count % 10 == 0)
-//			{
-//				printf("\n");
-//			}
-//		}
-//	}
-//}
+#include <stdio.h>
+
+int main()
+{
+	int number;
+	int prime;
+	int count=0;
+	printf("Â¼Ã’Â¼Ã¶Â¸Â¦ ÃƒÂ£Ã€Â» Â¹Ã¼Ã€Â§Â¸Â¦ Ã€Ã”Â·Ã‚: ");
+	scanf_s("%d", &number);
+	for (int i = 2; i <= number; i++) {
+		prime = 1;
+		for (int j = 2; j < i; j++) {
+			if (i % j == 0) {
+				prime = 0;
+				break;
+			}
+		}
+		if (prime == 1) {
+			printf("%d\t", i);
+			count++;
+			if (count % 10 == 0)
+			{
+				printf("\n");
+			}
+		}
+	}
+}
